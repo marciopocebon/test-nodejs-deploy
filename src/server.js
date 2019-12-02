9596b2bf10f9cdb205c40ff03919580b21356b44
Diego Fernandes <diego.schell.f@gmail.com>
2019-12-02 16:30:46 -0300

    feat: Setup deploy configuration

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+require('dotenv/config'); 
+
 const express = require('express');
 const routes = require('./routes');
 
@@ -6,6 +8,6 @@ require('./database');
 const app = express();
 
 app.use(express.json());
-app.use(routes);
+app.use(routes); 
 
-app.listen(3333);
+app.listen(process.env.PORT || 3333);
